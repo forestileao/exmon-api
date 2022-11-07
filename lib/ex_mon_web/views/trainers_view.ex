72b610fec5da961b2f9ee116ee9b59ff55dd2bac
pedro leao <pedroforestileao@gmail.com>
2022-11-06 22:05:17 -0300

    created crud for trainer and created pokemon import and schema

--- lib/ex_mon_web/views/trainers_view.ex
+++ lib/ex_mon_web/views/trainers_view.ex
@@ -4,12 +4,34 @@ defmodule ExMonWeb.TrainersView do
 
   def render("create.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
     %{
-      message: "Trainer Created",
+      message: "Trainer created",
       trainer: %{
         id: id,
         name: name,
         inserted_at: inserted_at
       }
+    }
+  end
+
+  def render("update.json", %{
+        trainer: %Trainer{id: id, name: name, inserted_at: inserted_at, updated_at: updated_at}
+      }) do
+    %{
+      message: "Trainer updated!",
+      trainer: %{
+        id: id,
+        name: name,
+        inserted_at: inserted_at,
+        updated_at: updated_at
+      }
+    }
+  end
+
+  def render("show.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+    %{
+      id: id,
+      name: name,
+      inserted_at: inserted_at
     }
   end
 end
