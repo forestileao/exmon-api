2e123e54ab3a6f684c91b896c459699e23922f8a
Pedro Foresti Leão <hellhat@pop-os.localdomain>
2022-12-17 18:54:17 -0300

    test: Added trainers controller tests

--- test/ex_mon/trainer/create_test.exs
+++ test/ex_mon/trainer/create_test.exs
@@ -3,4 +3,32 @@ defmodule ExMon.Trainer.CreateTest do
 
   alias ExMon.{Repo, Trainer}
   alias Trainer.Create
+
+  describe "call/1" do
+    test "when all params are valid, creates a trainer" do
+        params = %{name: "Pedro", password: "123456"}
+
+        count_before = Repo.aggregate(Trainer, :count)
+
+        response =  Create.call(params)
+
+        count_after = Repo.aggregate(Trainer, :count)
+
+        assert {:ok, %Trainer{name: "Pedro"}} = response
+        assert count_after > count_before
+    end
+
+    test "when any params are invalid, returns an error" do
+      params = %{name: "Pedro"}
+
+      count_before = Repo.aggregate(Trainer, :count)
+
+      response =  Create.call(params)
+
+      count_after = Repo.aggregate(Trainer, :count)
+
+      assert {:error, _changeset} = response
+      assert count_after == count_before
+  end
+  end
 end
---
+++ test/ex_mon_web/controllers/trainers_controller_test.exs
@@ -0,0 +1,30 @@
+defmodule ExMonWeb.TrainersControllerTest do
+  alias Erl2ex.Pipeline.ExMacro
+  use ExMonWeb.ConnCase
+
+  alias ExMon.Trainer
+
+  describe "show/2" do
+    test "when there is a trainer with the given id, returns the trainer", %{conn: conn} do
+      params = %{name: "Pedro", password: "123456"}
+
+      {:ok, %Trainer{id: id}} = ExMon.create_trainer(params)
+
+      response =
+        conn
+        |> get(Routes.trainers_path(conn, :show, id))
+        |> json_response(:ok)
+
+      assert %{"id" => ^id, "inserted_at" => _inserted_at, "name" => "Pedro"} = response
+    end
+
+    test "when there is an error, returns the error", %{conn: conn} do
+      response =
+        conn
+        |> get(Routes.trainers_path(conn, :show, "1234"))
+        |> json_response(:bad_request)
+
+      assert %{"message" => "Invalid ID format!"} == response
+    end
+  end
+end
